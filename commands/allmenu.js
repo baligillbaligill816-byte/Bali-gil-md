fdf03d022e4274b5b11c240a7543aee21dfa272c
itachi-uchia34 <ali00sona1@gmail.com>
2026-08-12 16:10:45 +0000

    Delete commands/allmenu.js

--- commands/allmenu.js
+++
@@ -1,8 +0,0 @@
-const { buildFullMenuText } = require('../lib/menu');
-
-async function allMenu(sock, from, msg, session = {}, commands = {}) {
-    const menuText = buildFullMenuText(session, commands);
-    await sock.sendMessage(from, { text: menuText }, { quoted: msg });
-}
-
-module.exports = allMenu;
