00409de002dbac4281d734c8904f8d090a1ced74
itachi-uchia34 <ali00sona1@gmail.com>
2026-08-12 20:53:41 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
     startimage: 'https://lh3.googleusercontent.com/d/18DX3oRbQ1E__z-64MUMOM-mRtmSfsLqr',
-    ownerNumber: process.env.OWNER_NUMBER || '+923416181562',
+    ownerNumber: process.env.OWNER_NUMBER || '+923416181562','923110470403'
     botName: 'BALI GIL MINI BOT',
     ownerName: 'ITACHI-UCHIHA',
     // Keep old name for backward compatibility
